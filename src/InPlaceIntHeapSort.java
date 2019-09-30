44d7995c5d5b1ed3418ff0c33c06b3710b9359df
Spencer6497 <peaces2@mymail.nku.edu>
2019-09-30 12:38:20 -0400

    Flesh out header comments

--- src/InPlaceIntHeapSort.java
+++ src/InPlaceIntHeapSort.java
@@ -1,5 +1,15 @@
+/*
+    Spencer Peace
+    CSC 364-001
+    HW3: Sorting
+    Dr. Jeff Ward
+
+    This program provides a method for implementing a heap sort "in-place" by not allocating a heap object to reduce
+    memory waste, and instead using a small number of local variables to perform the operation.
+ */
+
 public class InPlaceIntHeapSort {
-    public static void main(String[] args) {
+    public static void heapSort(int[] array) {
 
     }
 }
